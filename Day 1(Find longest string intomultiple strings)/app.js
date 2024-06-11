7badf76c74d75c3287de8ee9fd1688770353b086
waasimAnsari786 <waasimansari92@gmail.com>
2024-06-10 20:28:29 -0700

    Update app.js
    I added an input so that user can interact better with my app.

--- Day 1(Find longest string intomultiple strings)/app.js
+++ Day 1(Find longest string intomultiple strings)/app.js
@@ -1,3 +1,17 @@
+let body = document.querySelector("body");
+body.innerHTML = `  <h1>Find Longest String</h1>
+
+                    <form>
+                        <input type='text' placeholder='Enter Your text'>
+                    </form>
+                    
+                    <p></p>
+`;
+
+let inp = document.querySelector('input');
+let msg = document.querySelector("p");
+let form = document.querySelector("form");
+
 const findLongestStr = (str) => {
     if (str) {
         let trimmedStr = str.trim();
@@ -15,7 +29,15 @@ const findLongestStr = (str) => {
     return false;
 };
 
-// Test the function
-console.log(findLongestStr('      = ddefcvf cmdj 4884 4 true !@# @#$ +)(^&^( ccvdvbf d7644cvfvbjho vsdfvs849c _)(*&)dgddg6486^&^*()        ')); // Expected output: d7644cvfvbjho
-console.log(findLongestStr('        ')); // Expected output: false
-console.log(findLongestStr('')); // Expected output: false
+form.addEventListener("submit" , (evt) => {
+    evt.preventDefault();
+    let text = findLongestStr(inp.value);
+
+    if (text) {
+        msg.innerText = `The longest string is ${text}`;
+    }
+
+    else{
+        msg.innerText = 'Your entered string is incorrect';
+    }
+});
