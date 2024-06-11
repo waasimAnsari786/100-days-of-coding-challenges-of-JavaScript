5483cb8569cec78055f13ce7b8f75fcda3d08475
waasimAnsari786 <waasimansari92@gmail.com>
2024-06-10 20:15:11 -0700

    Update app.js
    I added an input , a button fort copying the generated hashtag to make better the user experience.

--- Day 2(Hashtag Generator)/app.js
+++ Day 2(Hashtag Generator)/app.js
@@ -1,3 +1,20 @@
+let body = document.querySelector("body");
+body.innerHTML = `  <h1>Hashtag Generator</h1>
+
+                    <form>
+                        <input type='text' placeholder='Enter Your text'>
+                    </form>
+                    
+                    <h3></h3>
+
+                    <button>copy hashtag</button>
+`;
+
+let inp = document.querySelector('input');
+let msg = document.querySelector("h3");
+let form = document.querySelector("form");
+let btn = document.querySelector("button");
+
 const hashtagGenratorFunc = (str) => {
     let trimmedStr = str.toString().trim();
 
@@ -14,4 +31,14 @@ const hashtagGenratorFunc = (str) => {
     };
 };
 
-console.log(hashtagGenratorFunc(2024));
+const copyHashtagFunc = () => {
+    navigator.clipboard.writeText(msg.innerText);
+    alert(`Text Copied to the clipboard ${msg.innerText}`);
+};
+
+form.addEventListener("submit" , (evt) => {
+    evt.preventDefault();
+    msg.innerText = hashtagGenratorFunc(inp.value);
+});
+
+btn.addEventListener("click" , copyHashtagFunc);
