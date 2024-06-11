0c56abe9a2f6eb27a8e17b507d1d0d6a6ded16cb
waasimAnsari786 <waasimansari92@gmail.com>
2024-06-10 19:31:39 -0700

    Update app.js
    I have completed Day 2 challenge.

--- Day 2(Hashtag Generator)/app.js
+++ Day 2(Hashtag Generator)/app.js
@@ -1,1 +1,17 @@
-const hashtagGenratorFunc = () => {};
+const hashtagGenratorFunc = (str) => {
+    let trimmedStr = str.trim();
+
+    if (trimmedStr.length > 0) {
+        let splitedStr = trimmedStr.split(" ");
+        let toUpperCaseStr = splitedStr.map(currElem => currElem.replace(currElem[0] , currElem[0].toUpperCase()));
+        let joinedArr = toUpperCaseStr.join("");
+
+        return `#${joinedArr}`;
+    }
+
+    else{
+        return false
+    };
+};
+
+console.log(hashtagGenratorFunc('ready for t20 world cup 2024'));
