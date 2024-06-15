bc5042b289e35eb66128cf53d9c576114ede0b84
waasimAnsari786 <waasimansari92@gmail.com>
2024-06-14 22:37:53 -0700

    I have completed challenge day 6

--- Day 6(Find out Palindrome)/app.js
+++ Day 6(Find out Palindrome)/app.js
@@ -1,10 +1,35 @@
-let arr = [1 , 8 ,9 ,7 , 2 , 3];
-let arr2 = [10 , 108 , 59 , 7 , 2 , 13];
+let form = document.createElement('form');
+form.innerHTML = `
+                    <h1 class="heading">palidrome text finder</h1>
+                    <input type="text" placeholder="Enter Your Text">
+                    <p class="mt-3"></p>`;
 
-const sortOutArray = (arr) => {
-    arr.sort((a,b) => a - b)
-    return arr;
+let body = document.querySelector("body");
+body.innerHTML = '<div class="container main-div"></div>';
+
+let ctnr = document.querySelector(".container");
+ctnr.prepend(form)
+
+let inp = document.querySelector("input");
+let msg = document.querySelector("p");
+
+const isPalindrome = (str) => {
+    let filteredStr = str.toUpperCase().split("").filter(currElem => currElem.charCodeAt() >= 65 && currElem.charCodeAt() <= 90 ? currElem : "");
+    let joinedStr = filteredStr.reverse().join("");
+
+    return [joinedStr , filteredStr.reverse().join("")];
 };
 
-console.log(sortOutArray(arr));
-console.log(sortOutArray(arr2));
+form.addEventListener("submit" , (evt) => {
+    evt.preventDefault();
+    let text = isPalindrome(inp.value);
+    let [joinedStr , filteredStr] = text;
+
+    if (joinedStr === filteredStr) {
+        msg.innerText = `After filtering ${joinedStr} i'm telling you that your entered string ${inp.value} is Palindrome`;
+    }
+
+    else{
+        msg.innerText = `After filtering ${joinedStr} i'm telling you that your entered string ${inp.value} is not Palindrome`;
+    }
+});
